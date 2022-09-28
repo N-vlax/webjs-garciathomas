79488db27da367a1adb7bf654cb19e95d7321bd9
Thomas <thomas30250@gmail.com>
2022-09-28 14:46:50 +0200

    console log qui marche et ajout div

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+const tCheck = document.getElementById('container')
+
+console.log (tCheck)
 
 const newText = document.createElement("div");
 
-let essai = document.getElementsByClassName("container");
+let essai = document.getElementById("container");
 
 essai.appendChild(newText);
 
